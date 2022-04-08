11f6feb8822bf6a530e6290019ea36cfd22b00aa
lebrancconvas <lebrancconvas@gmail.com>
2022-04-08 12:42:00 +0700

    :truck: Change Name.

--- CataParuto/CataParuto/CataParuto.cs
+++ CataParuto/CataParuto/CataParuto.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace CataParuto
 {
-    public class Game1 : Game
+    public class CataParuto : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1()
+        public CataParuto()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
--- CataParuto/CataParuto/Program.cs
+++ CataParuto/CataParuto/Program.cs
@@ -7,7 +7,7 @@ namespace CataParuto
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new CataParuto())
                 game.Run();
         }
     }
